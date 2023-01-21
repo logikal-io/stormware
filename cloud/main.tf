a94d7d45a7327ead40a603915172357a0bb3d8da
GergelyKalmar <gergely.kalmar@logikal.io>
2023-01-21 12:39:51 +0100

    Add permissions

--- cloud/main.tf
+++ cloud/main.tf
@@ -25,7 +25,7 @@ module "aws_github_auth" {
 
   project_id = var.project_id
   role_accesses = {
-    testing = ["logikal-io/mindlab"]
+    testing = ["logikal-io/stormware"]
   }
 }
 
@@ -50,4 +50,52 @@ resource "google_bigquery_dataset" "test" {
   location = "EU"
 
   depends_on = [google_project_service.bigquery]
+}
+
+# Permissions
+resource "google_project_iam_member" "secret_adder" {
+  project = var.project_id
+  role = "roles/secretmanager.secretVersionAdder"
+  member = "serviceAccount:${module.gcp_github_auth.service_account_emails["testing"]}"
+
+  condition {
+    title = "test secret"
+    expression = "resource.name == '${google_secret_manager_secret.test.name}'"
+  }
+}
+
+resource "google_project_iam_member" "secret_accessor" {
+  project = var.project_id
+  role = "roles/secretmanager.secretAccessor"
+  member = "serviceAccount:${module.gcp_github_auth.service_account_emails["testing"]}"
+
+  condition {
+    title = "test secret"
+    expression = "resource.name == '${google_secret_manager_secret.test.name}'"
+  }
+}
+
+resource "google_project_iam_member" "bigquery_job_user" {
+  project = var.project_id
+  role = "roles/bigquery.jobUser"
+  member = "serviceAccount:${module.gcp_github_auth.service_account_emails["testing"]}"
+}
+
+data "aws_iam_policy_document" "test_secret_access" {
+  version = "2012-10-17"
+
+  statement {
+    actions = ["secretsmanager:PutSecretValue", "secretsmanager:GetSecretValue"]
+    resources = [aws_secretsmanager_secret.test.arn]
+  }
+}
+
+resource "aws_iam_policy" "test_secret_access" {
+  name = "test-secret-access"
+  policy = data.aws_iam_policy_document.test_secret_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "test_secret_access" {
+  role = module.aws_github_auth.iam_role_names["testing"]
+  policy_arn = aws_iam_policy.test_secret_access.arn
 }
