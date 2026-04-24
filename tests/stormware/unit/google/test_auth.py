9cdc5ccf2853c282047513d631c807080175d804
GergelyKalmar <gergely.kalmar@logikal.io>
2026-04-24 21:48:17 +0200

    Fix credentials

--- tests/stormware/unit/google/test_auth.py
+++ tests/stormware/unit/google/test_auth.py
@@ -25,12 +25,14 @@ def test_project_error(mocker: MockerFixture) -> None:
 
 
 def test_credentials(mocker: MockerFixture, tmp_path: Path) -> None:
+    tool_config = mocker.patch('stormware.google.auth.tool_config', return_value={})
+    mocker.patch('stormware.google.auth.xdg_config_home', return_value=tmp_path)
+
+    # Credentials mocks
     oauth2_credentials = mocker.Mock(name='oauth2_credentials', scopes=['test'])
     impersonated_credentials = mocker.Mock(name='impersonated_credentials')
     default_credentials = mocker.Mock(name='default_credentials', scopes=['test'])
 
-    tool_config = mocker.patch('stormware.google.auth.tool_config', return_value={})
-    mocker.patch('stormware.google.auth.xdg_config_home', return_value=tmp_path)
     credentials_path = tmp_path / 'gcloud/credentials/example-org.json'
     credentials_path.parent.mkdir(parents=True, exist_ok=True)
     credentials_path.write_text('{}')
@@ -40,6 +42,7 @@ def test_credentials(mocker: MockerFixture, tmp_path: Path) -> None:
     mocker.patch('stormware.google.auth.impersonated_credentials.Credentials',
                  return_value=impersonated_credentials)
     mocker.patch('stormware.google.auth.default', return_value=[default_credentials, None])
+
     auth = GCPAuth(scopes=['test'])
 
     # Organization credentials
@@ -58,13 +61,24 @@ def test_credentials(mocker: MockerFixture, tmp_path: Path) -> None:
 
 
 def test_scoped_credentials(mocker: MockerFixture, tmp_path: Path) -> None:
-    default_credentials = mocker.Mock(name='default_credentials', scopes=None)
-    user_credentials = mocker.Mock(name='user_credentials')
-    user_credentials.to_json = lambda: '{"token": "user_credentials"}'
-    mocker.patch('stormware.google.auth.GCPAuth.credentials_path', return_value=None)
-    mocker.patch('stormware.google.auth.default', return_value=[default_credentials, None])
-    mocker.patch('stormware.google.auth.get_user_credentials', return_value=user_credentials)
+    # Credential mocks
+    mocker.patch('stormware.google.auth.tool_config', return_value={})
     mocker.patch('stormware.google.auth.xdg_config_home', return_value=tmp_path)
+
+    oauth2_credentials = mocker.Mock(name='oauth2_credentials', scopes=['test'])
+    user_info = mocker.patch('stormware.google.auth.OAuth2Credentials.from_authorized_user_info')
+    user_info_with_project = user_info.return_value.with_quota_project
+    user_info_with_project.return_value = oauth2_credentials
+
+    user_credentials = mocker.Mock(name='user_credentials', scopes=['test'])
+    user_credentials.to_json = lambda: '{"token": "user_credentials"}'
+    mocker.patch('stormware.google.auth.get_user_credentials', return_value=user_credentials)
+
+    credentials_path = tmp_path / 'gcloud/credentials/example-org.json'
+    credentials_path.parent.mkdir(parents=True, exist_ok=True)
+    credentials_path.write_text('{}')
+
+    # Secret mocks
     secrets = {
         GCPAuth.DEFAULT_OAUTH_CLIENT_SECRETS_KEY: '{"client_id": "id", "client_secret": "secret"}',
         GCPAuth.DEFAULT_OAUTH_CREDENTIALS_KEY: None,
@@ -75,10 +89,10 @@ def test_scoped_credentials(mocker: MockerFixture, tmp_path: Path) -> None:
 
     # Non-interactive flow error
     logger.info('Testing non-interactive session flow error')
-    auth = GCPAuth(scopes=['test'], ignore_cached_oauth_credentials=True)
+    auth = GCPAuth(scopes=['test', 'scope'], ignore_cached_oauth_credentials=True)
     is_tty.return_value = False
     with raises(RuntimeError, match='not an interactive session'):
-        auth.credentials(organization='example.org', scopes=['test'])
+        auth.credentials(organization='example.org', scopes=['scope'])
 
     # Successful flow
     logger.info('Testing successful flow while ignoring cache load')
@@ -88,11 +102,11 @@ def test_scoped_credentials(mocker: MockerFixture, tmp_path: Path) -> None:
 
     # Cache load (Secret Manager)
     logger.info('Testing Secret Manager cache load')
-    auth = GCPAuth(scopes=['test'])
-    cached_credentials = mocker.Mock(name='cached_credentials')
-    user_info = mocker.patch('stormware.google.auth.OAuth2Credentials.from_authorized_user_info')
+    auth = GCPAuth(scopes=['test', 'scope'])
+    cached_credentials = mocker.Mock(name='cached_credentials', scopes=['scope'])
+    cached_credentials.with_quota_project.return_value = user_credentials
     user_info.return_value = cached_credentials
-    assert auth.credentials(organization='example.org', scopes=['test']) == cached_credentials
+    assert auth.credentials(organization='example.org', scopes=['scope']) == cached_credentials
     user_info.assert_called_with(info={'token': 'user_credentials'})  # nosec: test case
 
     # Cache load (local cache)
